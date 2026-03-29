66a741434a855b6ebfaa3e37811f54b7ea10cb2a
Sunny <sunnysinghhrana@gmail.com>
2026-03-29 23:49:05 +0530

    -add dummy services -add login & register pages

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,28 +1,30 @@
 const services = [
-  { title: "Electrician", price: 500, location: "Hyderabad" },
-  { title: "Plumber", price: 300, location: "Chennai" },
-  { title: "Math Tutor", price: 700, location: "Bangalore" },
-  { title: "AC Repair", price: 600, location: "Delhi" }
+  { id: 1, title: "Electrician", price: 500, location: "Hyderabad" },
+  { id: 2, title: "Plumber", price: 300, location: "Chennai" },
+  { id: 3, title: "Math Tutor", price: 700, location: "Bangalore" },
+  { id: 4, title: "AC Repair", price: 600, location: "Delhi" }
 ];
 
 const container = document.getElementById("services-container");
 
-services.forEach(service => {
-  const card = document.createElement("div");
-  card.className = "card";
+if (container) {
+  services.forEach(service => {
+    const card = document.createElement("div");
+    card.className = "card";
 
-  card.innerHTML = `
-    <h3>${service.title}</h3>
-    <p>₹${service.price}</p>
-    <p>${service.location}</p>
-    <button onclick="viewService(${service.id})">View Details</button>
-  `;
+    card.innerHTML = `
+      <h3>${service.title}</h3>
+      <p>₹${service.price}</p>
+      <p>${service.location}</p>
+      <button onclick="viewService(${service.id})">View Details</button>
+    `;
 
-  container.appendChild(card);
-});
+    container.appendChild(card);
+  });
+}
 
 function viewService(id) {
-  window.location.href = `pages/service.html?id=${id}`;
+  window.location.href = `./pages/service.html?id=${id}`;
 }
 
 function getServiceIdFromURL() {
@@ -42,6 +44,8 @@ function loadServiceDetails() {
   document.getElementById("title").innerText = service.title;
   document.getElementById("price").innerText = "₹" + service.price;
   document.getElementById("location").innerText = service.location;
+  console.log("LOADING SERVICE PAGE");
+  console.log("ID:", id);
 }
 if (window.location.pathname.includes("service.html")) {
   loadServiceDetails();
