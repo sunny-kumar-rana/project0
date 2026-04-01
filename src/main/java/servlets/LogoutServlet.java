4129808f7f6ea343f6e2902a048c2ee1e47a513e
Sunny <sunnysinghhrana@gmail.com>
2026-04-01 23:26:30 +0530

    - add LogoutServlet

---
+++ src/main/java/servlets/LogoutServlet.java
@@ -0,0 +1,19 @@
+package servlets;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+
+        request.getSession().invalidate();
+        response.setStatus(200);
+    }
+}
