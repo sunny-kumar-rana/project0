60d5ca1952fc7f0225c1f7e077d1ab426b590e52
Sunny <sunnysinghhrana@gmail.com>
2026-04-04 22:09:34 +0530

    - add DBConnection.java class

---
+++ src/main/java/DBConnectivity/DBConnection.java
@@ -0,0 +1,16 @@
+package DBConnectivity;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+    private static final String USER = "project0";
+    private static final String PASSWORD = "Admin";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
